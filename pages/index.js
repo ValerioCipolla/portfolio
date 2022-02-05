95acf16afd8e60cfa586c27f9f85ffa9bab87103
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-05 15:33:52 +0000

    added fixed position to navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <br />
+      <br />
+      <br /> <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      LoremIpsumLoremIpsum <br />
+      LoremIpsumLore <br />
+      mIpsumLoremIpsumLoremIpsumLoremIpsumL <br />
+      oremIpsumLore <br />
+      mIpsumLoremIpsumL <br />
+      oremIpsumo <br />
+      remI <br /> <br />
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+    </div>
+  );
 }

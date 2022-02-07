082d15f15d913a4fcb24e7bb8e5a7852caaf8efb
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-07 11:38:44 +0000

    changed about

--- pages/about.js
+++ pages/about.js
@@ -18,16 +18,13 @@ const About = () => {
         easy-to-use interfaces. I became interested in Software engineering in
         May 2021, and I have been studying and improving since then. I started
         by following the freeCodeCamp curriculum and completing the first 2
-        certifications -
-        <span className={css.blueText}> Responsive Web Design </span> and
+        certifications - Responsive Web Design and JavaScript Algorithms and
+        Data Structure - after that I knew I was in love with JavaScript and
+        wanted to become a full-time Software Engineer. I kept improving and
+        learning through multiple Udemy courses and in November 2021 I decided
+        to change my life forever and start a{" "}
         <span className={css.greenText}>
-          {" "}
-          JavaScript Algorithms and Data Structure{" "}
-        </span>
-        - after that I knew I was in love with JavaScript. I kept improving and
-        learning through multiple Udemy courses and in November 2021 I started a{" "}
-        <span className={css.emphasis}>
-          4 months Full-Stack Software Development Bootcamp
+          16 weeks Full-Stack Software Development Bootcamp
         </span>{" "}
         with{" "}
         <a href="https://www.schoolofcode.co.uk/" target="_blank">
@@ -36,15 +33,33 @@ const About = () => {
         .
       </p>
       <p>
-        The bootcamp has been amazing and I love learning new things everyday, I
-        am looking forward to starting my career as a Software Engineer to work
-        on real-world problems and potentially improve people's lives through
-        software.
+        The bootcamp has been amazing and I genuinely feel like what I have
+        learnt in 4 months at the Bootcamp would have taken me 18/24 months of
+        studying on my own! On the course we learnt how to build{" "}
+        <span className={css.blueText}>full-stack applications</span>, test, and
+        deploy them with the help of technologies like: Git, HTML, CSS,
+        JavaScript, React JS, Node.js, TypeScript, PostgreSQL, Jest, Cypress,
+        Docker, Heroku, Netlify and many more.
       </p>
       <p>
-        I am a naturally curious person and obsessed with "self-improvement", I
-        am always learning something new. Before finding my love for coding I
-        was a certified Personal Trainer and Sports Massage Therapist, and in
+        The course was 100% remote and it didn't just teach me how to code, but
+        it also taught me how to work remotely and effectively within a team. A
+        lot of emphasis was put on learning{" "}
+        <span className={css.greenText}>Agile Principles</span> which really
+        paid off during the week 9 project, and the final 4 weeks-long project.{" "}
+        <br />
+        All of the learning and building during the course was done using{" "}
+        <span className={css.blueText}>Pair-Programming</span> and Git for
+        version control, including branching, pull requests, merge requests and
+        solving conflicts.
+      </p>
+      <p>
+        I am looking forward to starting my career as a Software Engineer to
+        work on <span className={css.greenText}>real-world problems</span> and
+        potentially improve people's lives through software. Naturally curious
+        and obsessed with <span className={css.blueText}>self-improvement</span>
+        , I am always learning something new. Before finding my love for coding
+        I was a certified Personal Trainer and Sports Massage Therapist, and in
         2020 I earned a Bachelor's degree in Psychology. I am confident the
         people skills I gained through my previous employment and my knowledge
         about the human mind and body will prove very valuable in my engineering

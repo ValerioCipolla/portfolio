23eef1d172b485c47dc433b08f327100c52e5e4f
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-07 08:18:45 +0000

    added icons

--- Components/Sidebar/index.js
+++ Components/Sidebar/index.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import * as css from "../../styles/sidebar.module.css";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 const Sidebar = () => {
-  return <div>this is a sidebar</div>;
+  return (
+    <div className={css.sidebar}>
+      <div className={css.icon}>
+        <BsGithub />
+      </div>
+      <div className={css.icon}>
+        <BsLinkedin />
+      </div>
+      <div className={css.icon}>
+        <MdEmail />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

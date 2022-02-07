a3fe797ad1d2a14d55f88e9d5770c514c385167e
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-07 08:53:20 +0000

    added responsiveness to homepage

--- Components/Sidebar/index.js
+++ Components/Sidebar/index.js
@@ -2,29 +2,36 @@ import React from "react";
 import * as css from "../../styles/sidebar.module.css";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
+import { useWindowSize } from "../../hooks/useWindowSize";
 const Sidebar = () => {
-  return (
-    <div className={css.sidebar}>
-      <div className={css.icon}>
-        <a href="https://github.com/ValerioCipolla" target="_blank">
-          <BsGithub />
-        </a>
+  const windowSize = useWindowSize();
+
+  if (windowSize.width > 700) {
+    return (
+      <div className={css.sidebar}>
+        <div className={css.icon}>
+          <a href="https://github.com/ValerioCipolla" target="_blank">
+            <BsGithub />
+          </a>
+        </div>
+        <div className={css.icon}>
+          <a
+            href="https://www.linkedin.com/in/valerio-cipolla-software-engineer/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+        </div>
+        <div className={css.icon}>
+          <a href="mailto: valeriocipolla92@gmail.com" target="_blank">
+            <MdEmail />
+          </a>
+        </div>
       </div>
-      <div className={css.icon}>
-        <a
-          href="https://www.linkedin.com/in/valerio-cipolla-software-engineer/"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-      </div>
-      <div className={css.icon}>
-        <a href="mailto: valeriocipolla92@gmail.com" target="_blank">
-          <MdEmail />
-        </a>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <div className={css.hidden}></div>;
+  }
 };
 
 export default Sidebar;

142384b7ee845526680d7cf2f684608f20129385
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-14 08:58:57 +0000

    added aos animation to index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import * as css from "../styles/index.module.css";
 import Button from "../Components/Button";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home({ setActivePage }) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className={css.welcome}>
+    <div className={css.welcome} data-aos="fade-down">
       <h1>Valerio Cipolla.</h1>
       <h2>Software Engineer & Full Stack Developer</h2>
       <div className={css.buttonSection}>

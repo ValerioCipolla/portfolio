25d9f824d35b675ed2173f92acecb48e28ac29a0
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-20 15:26:40 +0000

    added express to tech stack in projects

--- data/data.js
+++ data/data.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "School Of Code Link Tracker",
     liveLink: "https://soc-links-tracker.herokuapp.com/",
     sourceLink: "https://github.com/ValerioCipolla/soc-link-tracker-full-stack",
-    techStack: "React JS, CSS, Node.js, PostgreSQL",
+    techStack: "React JS, CSS, Node.js, Express, PostgreSQL",
     description:
       "I worked on this project as a member of a team of 4 people. We were asked by SchoolOfCode to solve a problem that we encountered through the bootcamp. We thought that one of the most difficult things for us to do up until that point was to keep track of all the resources that we were shown on a daily basis! That's why we decided to create something that would help us do that. We focused on usability and accessibility, that meant creating an easy-to-use and functional user interface with React which delivered an excellent UX. Most of the time on this project I spent it looking after the back-end part (setting up the PostgreSQL database on Heroku and creating the API that serves the front-end with Node.js) and deploying the application. I also created the form to add new resources to the database using React. During this project I worked on my back-end and front-end skills, deployed a full-stack application for the first time and understood what it means to work in a team and cooperate using version control (git branching, pull requests, merge requests and code reviews).",
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "myQuotes Collection",
     liveLink: "https://quote-generator-full-stack.herokuapp.com/",
     sourceLink: "https://github.com/ValerioCipolla/quote-collection-full-stack",
-    techStack: "JavaScript, HTML, CSS, Node.js, PostgreSQL",
+    techStack: "JavaScript, HTML, CSS, Node.js, Express, PostgreSQL",
     description:
       "Halfway through the SchoolOfCode Bootcamp, we had Christmas holidays, I could not afford to stop coding for 2 weeks so I decided to work on a solo project to cement all of the knowledge acquired in the previous 6 weeks. This is the first full-stack application I ever built, I started by setting up and populating a database with quotes that I like, I then built the server and the CRUD API that would allow me to interact with the database (getting quotes by id, or author, creating new quotes, updating or deleting existing quotes). The final days were dedicated to building the front-end, at the time I didn't know how to use React yet, so I had to use pure HTML, CSS and vanilla JS. Working on this project taught me how to work with CRUD API and databases, and I also used Nodemailer for the first time, to give my users the ability to e-mail quotes to whoever they like (themselves included). Looking at the future: I plan on changing the front-end to React JS as it would make my codebase a lot easier to navigate and understand, as well as making a better user experience.",
   },
